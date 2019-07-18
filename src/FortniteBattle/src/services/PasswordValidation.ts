d97d323a6eef6801b05a8976a82e3a7b1d779562
klaus97 <raffaele830@gmail.com>
2019-07-18 20:13:57 +0200

    update

--- src/FortniteBattle/src/services/PasswordValidation.ts
+++ src/FortniteBattle/src/services/PasswordValidation.ts
@@ -4,11 +4,9 @@ export class PasswordValidation {
     static MatchPassword(AC: AbstractControl) {
         const password = AC.get('password').value; // to get value in input tag
         const confirmPassword = AC.get('confirmpassword').value; // to get value in input tag
-        if(password !== confirmPassword) {
-            console.log('false');
+        if (password !== confirmPassword) {
             AC.get('confirmpassword').setErrors( {MatchPassword: true} );
         } else {
-            console.log('true');
             return null;
         }
     }
